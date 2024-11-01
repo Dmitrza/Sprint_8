7594348f0e0efa77e97aa3bd4b85a7b32d595d34
Dmitrii Zakharov <dmitrza1991@yandex.ru>
2024-11-01 20:23:11 +0100

    added token

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -10,7 +10,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class Mesto1Test {
 
-    String bearerToken = "сюда_впиши_свой_токен";
+    String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmRmMzFkNTBjODlhYTAwM2QzYzA3NGMiLCJpYXQiOjE3MzA0ODg5MTksImV4cCI6MTczMTA5MzcxOX0.y5Kw9Y9OtZvxT-eUVOCkhfP0-87-YTH9gJRG9bzY4Fs";
 
     @Before
     public void setUp() {
