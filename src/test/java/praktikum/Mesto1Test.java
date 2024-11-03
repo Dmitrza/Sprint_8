1dc40a18bf506236f952482430338a567c7020a8
Dmitrii Zakharov <dmitrza1991@yandex.ru>
2024-11-03 09:18:48 +0100

    added checkUserName test and package praktikum

--- src/test/java/praktikum/Mesto1Test.java
+++ src/test/java/praktikum/Mesto1Test.java
@@ -1,3 +1,5 @@
+package praktikum;
+
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
@@ -6,7 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class Mesto1Test {
 
@@ -15,6 +17,16 @@ public class Mesto1Test {
     @Before
     public void setUp() {
         RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
+    }
+
+    @Test
+    @DisplayName("Check user name")
+    @Description("This test is for check current user's name.")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .get("/api/users/me") // Делаем GET-запрос
+                .then().assertThat().body("data.name", equalTo("Incorrect Name")); // Проверяем, что имя соответствует ожидаемому
     }
 
     @Test
